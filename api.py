d1882d1f229bfb1bcf21229376349ab2cdbe54ae
unknown <mazvan2005@yahoo.com>
2026-03-17 21:52:46 +0200

    cors

--- api.py
+++ api.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 import joblib
 
 app = FastAPI()
 
 model = joblib.load('model.pkl')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all for now
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/predict")
 def predict(years:int):
